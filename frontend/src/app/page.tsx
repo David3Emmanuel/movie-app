a987ac8e6025501a181057faef02e0909c3a3376
David Emmanuel <david3emmanuel@gmail.com>
2024-11-17 23:08:15 +0100

    docker setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function HomePage() {
-  return 'Frontend Home Page'
+export default async function HomePage() {
+  let messageFromBackend: string
+
+  if (process.env.BACKEND_URL === undefined) {
+    messageFromBackend = 'BACKEND_URL is not defined'
+  } else {
+    try {
+      const response = await fetch(process.env.BACKEND_URL)
+      messageFromBackend = await response.text()
+    } catch (error) {
+      console.error(error)
+      messageFromBackend = (error as Error).message
+    }
+  }
+
+  return (
+    <>
+      <p>Frontend Home Page</p>
+      <p>Backend URL - [{process.env.BACKEND_URL}]</p>
+      <p>Backend Message - {messageFromBackend}</p>
+    </>
+  )
 }
